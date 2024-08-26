b78ab49ac79bca3987466a92f4a30255237eba75
maly-phy <mohamed.aly@stud.uni-goettingen.de>
2024-08-26 16:50:47 +0200

    merging files from BERT-PAL to main

--- run_train.sh
+++ run_train.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=train-multitask_classifier
-#SBATCH -t 00:20:00                  # estimated time # TODO: adapt to your needs
+#SBATCH -t 15:00:00                  # estimated time # TODO: adapt to your needs
 #SBATCH -p grete                     # the partition you are training on (i.e., which nodes), for nodes see sinfo -p grete:shared --format=%N,%G
-#SBATCH -G A100:1                    # take 1 GPU, see https://docs.hpc.gwdg.de/compute_partitions/gpu_partitions/index.html for more options
+#SBATCH -G A100:4                    # take 1 GPU, see https://docs.hpc.gwdg.de/compute_partitions/gpu_partitions/index.html for more options
 #SBATCH --mem-per-gpu=8G             # setting the right constraints for the splitted gpu partitions
 #SBATCH --nodes=1                    # total number of nodes
 #SBATCH --ntasks=1                   # total number of tasks
-#SBATCH --cpus-per-task=8            # number cores per task
-#SBATCH --mail-type=all              # send mail when job begins and ends
-#SBATCH --mail-user=frederik.hennecke@stud.uni-goettingen.de
+#SBATCH --cpus-per-task=32            # number cores per task
+#SBATCH --mail-type=END              # send mail when job begins and ends
+#SBATCH --mail-user=mohamed.aly@stud.uni-goettingen.de
 #SBATCH --output=./slurm_files/slurm-%x-%j.out     # where to write output, %x give job name, %j names job id
 #SBATCH --error=./slurm_files/slurm-%x-%j.err      # where to write slurm error
 
+# srun -p grete:shared --pty -n 1 -C inet -c 8 -G A100:1 --interactive /bin/bash
 module load anaconda3
 source activate dnlp # Or whatever you called your environment.
+# conda install -y -c conda-forge spacy cupy spacy-transformers
+# pip install spacy-lookups-data
+# python -m spacy download en_core_web_sm
+
 
 # Printing out some info.
 echo "Submitting job with sbatch from directory: ${SLURM_SUBMIT_DIR}"
@@ -32,4 +37,6 @@ echo "Latest Commit: $(git rev-parse --short HEAD)"
 echo -e "Uncommitted Changes: $(git status --porcelain | wc -l)\n"
 
 # Run the script:
-python -u multitask_classifier.py --use_gpu --local_files_only --option finetune --task sst --hidden_dropout_prob 0.1
+# python -u train_multitask.py --use_gpu --local_files_only --option finetune --task multitask --hpo --smoketest --additional_inputs --profiler --sst --sts --para
+# python -u single_classify.py --use_gpu --local_files_only --option finetune --task sst
+python -u train_multitask_pal.py --option finetune --use_gpu --local_files_only --smoketest --no_tensorboard --no_train_classifier --use_smart_regularization --use_pal --use_amp
--- setup_gwdg.sh
+++ setup_gwdg.sh
@@ -28,7 +28,9 @@ check_conda_env() {
         echo "Conda environment 'dnlp' already exists."
     else
         echo "Conda environment 'dnlp' does not exist. Creating environment..."
-        conda create -n dnlp python=3.10 -y
+        # conda create -n dnlp python=3.10 -y
+        wget https://github.com/FrederikHennecke/DeepLearning4NLP/blob/main/dnlp.yml
+        conda env create -f dnlp.yml
     fi
 }
 
@@ -42,18 +44,16 @@ set -e
 eval "$(conda shell.bash hook)"
 
 echo "Activating conda environment 'dnlp'..."
-conda activate dnlp
+module load anaconda3
+source activate dnlp
 
 echo $CONDA_DEFAULT_ENV
 
-# Install packages
-conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
-conda install -y tqdm==4.66.2 requests==2.31.0 transformers==4.38.2 tensorboard==2.16.2 tokenizers==0.15.1 -c conda-forge -c huggingface
-pip install explainaboard-client==0.1.4 sacrebleu==2.4.0
-conda install -y scikit-learn==1.5.1
+## Download spacy POS and NER tags
+pip install "spacy[cuda-autodetect]>=3.5"
+python -m spacy download en_core_web_sm
 
-# Download model on login-node
-
+## Download model on login-node
 python - <<EOF
 from transformers import BertTokenizer, BertModel
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -61,11 +61,28 @@ model = BertModel.from_pretrained('bert-base-uncased')
 EOF
 
 python - <<EOF
-from transformers import AutoTokenizer, AutoModel, BartModel
+from transformers import BertTokenizer, BertModel
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
+model = BertModel.from_pretrained('bert-large-uncased')
+EOF
 
+python - <<EOF
+from transformers import RobertaTokenizer, RobertaModel
+tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+model = RobertaModel.from_pretrained('roberta-base')
+EOF
+
+
+python - <<EOF
+from transformers import AutoTokenizer, AutoModel, BartModel
 tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large')
 model = BartModel.from_pretrained('facebook/bart-large')
 EOF
 
 python -c "from tokenizer import BertTokenizer; from bert import BertModel; BertTokenizer.from_pretrained('bert-base-uncased'); BertModel.from_pretrained('bert-base-uncased')"
+python -c "from tokenizer import BertTokenizer; from bert import BertModel; BertTokenizer.from_pretrained('bert-large-uncased'); BertModel.from_pretrained('bert-large-uncased')"
+# python -c "from transformers import BertTokenizer; BertTokenizer.from_pretrained('bert-large-uncased'); from transformers import BertModel; BertModel.from_pretrained('bert-large-uncased')"
+
+
+python -c "from transformers import RobertaTokenizer; RobertaTokenizer.from_pretrained('roberta-base'); from transformers import RobertaModel; RobertaModel.from_pretrained('roberta-base')"
 python -c "from transformers import AutoTokenizer, AutoModel; AutoTokenizer.from_pretrained('facebook/bart-large'); from transformers import BartModel; BartModel.from_pretrained('facebook/bart-large')"
